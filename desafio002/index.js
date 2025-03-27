fc016bbfa66627ce51d9072719dc7fc977cdd310
AmandaJacobino <AmandaJacobino@outlook.com>
2025-03-27 16:52:44 -0300

    Organizando o programa principal na função main

--- desafio002/index.js
+++ desafio002/index.js
@@ -35,44 +35,46 @@ function calculo (v,d) {
     return v - d; 
 }
 
-//Estrutura de repetição
-while (true) {
-    //Entrada do nome do herói
-    let nome = prompt(`Digite o nome do herói (ou 'sair' para encerrar o programa): `);
-
-    //Condição para o usuário encerrar o programa
-    if (nome.toLowerCase() === "sair") {
-        console.log("Programa encerrado");
-        break;
+function main () {
+    //Estrutura de repetição
+    while (true) {
+        //Entrada do nome do herói
+        let nome = prompt(`Digite o nome do herói (ou 'sair' para encerrar o programa): `);
+    
+        //Condição para o usuário encerrar o programa
+        if (nome.toLowerCase() === "sair") {
+            console.log("Programa encerrado");
+            break;
+        }
+    
+        //Entrada da quantidade de vitoria e derrota
+        let vitoria = parseInt(prompt(`Digite a quantidade de vitórias: `));
+        let derrota = parseInt(prompt(`Digite a quantidade de derrotas: `));
+    
+        //Chamando a função, atribuindo os parâmetros e declarando a variável 'saldo'
+        let saldo = calculo(vitoria, derrota);
+    
+        //variavel nível
+        let nivel;
+    
+        //Verificação e classificação do nível do herói
+        if (saldo < 10) {
+            nivel = "Ferro";
+        } else if (saldo >= 11 && saldo <= 20) {
+            nivel = "Bronze";
+        } else if (saldo >= 21 && saldo <= 50) {
+            nivel = "Prata";
+        } else if (saldo >= 51 && saldo <= 80) {
+            nivel = "Ouro";
+        } else if (saldo >= 81 && saldo <= 90) {
+            nivel = "Diamante";
+        } else if (saldo >= 91 && saldo <= 100) {
+            nivel = "Lendário";
+        } else { //Para vitórias >= 101
+            nivel = "Imortal";
+        }
+    
+        //saída
+        console.log(`O herói tem saldo de ${saldo} vitórias e está no nível de ${nivel}`);
     }
-
-    //Entrada da quantidade de vitoria e derrota
-    let vitoria = parseInt(prompt(`Digite a quantidade de vitórias: `));
-    let derrota = parseInt(prompt(`Digite a quantidade de derrotas: `));
-
-    //Chamando a função, atribuindo os parâmetros e declarando a variável 'saldo'
-    let saldo = calculo(vitoria, derrota);
-
-    //variavel nível
-    let nivel;
-
-    //Verificação e classificação do nível do herói
-    if (saldo < 10) {
-        nivel = "Ferro";
-    } else if (saldo >= 11 && saldo <= 20) {
-        nivel = "Bronze";
-    } else if (saldo >= 21 && saldo <= 50) {
-        nivel = "Prata";
-    } else if (saldo >= 51 && saldo <= 80) {
-        nivel = "Ouro";
-    } else if (saldo >= 81 && saldo <= 90) {
-        nivel = "Diamante";
-    } else if (saldo >= 91 && saldo <= 100) {
-        nivel = "Lendário";
-    } else { //Para vitórias >= 101
-        nivel = "Imortal";
-    }
-
-    //saída
-    console.log(`O herói tem saldo de ${saldo} vitórias e está no nível de ${nivel}`);
 }
